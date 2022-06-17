d59dbe141b29c9f339902dea0828916bf3b11732
psd9988 <psd9988@gmail.com>
2022-06-17 19:50:38 +0530

    while

--- new.js
+++ new.js
@@ -1,21 +1,55 @@
 
 
-let a = process.argv[2];
+// let a = process.argv[2];
 
-for(let i = 1; i <= a; i++) {
-    console.log(i);
-}
+// for(let i = 1; i <= a; i++) {
+//     console.log(i);
+// }
 
-let b = process.argv[3];
+// let b = process.argv[3];
 
-for(let i = 1; i <= b; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    }
-}
+// for(let i = 1; i <= b; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
 
-let c = process.argv[4];
+// let c = process.argv[4];
 
-for(let i = c; i > 0; i--) {
-        console.log(i);
-    }
+// for(let i = c; i > 0; i--) {
+//         console.log(i);
+//     }
+
+
+
+// // using while loops
+
+
+// let d = process.argv[5];
+
+// let i = 1;
+
+//     while (i <= d) {
+//         console.log(i);
+//         i++;
+//     }
+
+
+// let e = process.argv[6];
+
+// let j = 1;
+
+// while(j<=e){
+//     if(j%2===0){
+//         console.log(j);
+//         j++;
+//     }
+// }
+
+
+// let f = process.argv[7];
+// let m = f;
+// while(m>0){
+//         console.log(m);
+//         m--;
+//     }
